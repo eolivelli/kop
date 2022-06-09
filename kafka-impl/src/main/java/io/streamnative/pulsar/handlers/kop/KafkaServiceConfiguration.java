6748dd2a153127b7c1c94468dd2e2e76a9b5fc57
Enrico Olivelli <eolivelli@apache.org>
2022-06-09 09:50:31 +0200

    add support for tlsHostnameVerificationEnabled

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaServiceConfiguration.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaServiceConfiguration.java
@@ -313,6 +313,18 @@ public class KafkaServiceConfiguration extends ServiceConfiguration {
     private String kopSslKeystoreType = "JKS";
 
     @FieldContext(
+            category = CATEGORY_KOP_SSL,
+            doc = "Use TLS while connecting to other brokers"
+    )
+    private boolean kopTlsEnabledWithBroker = false;
+
+    @FieldContext(
+            category = CATEGORY_KOP_SSL,
+            doc = "Enable hostname verification while connecting to other brokers"
+    )
+    private boolean tlsHostnameVerificationEnabled = false;
+
+    @FieldContext(
         category = CATEGORY_KOP_SSL,
         doc = "Kafka ssl configuration map with: SSL_KEYSTORE_LOCATION_CONFIG = \"ssl.keystore.location\""
     )
